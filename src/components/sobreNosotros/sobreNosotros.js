657d4e10ea5978bfb85a3a1c2aaecb1f5a8fa8f8
cl4ram <molinayvediaclara@outlook.com>
2021-11-28 00:36:47 -0300

    deploy

---
+++ src/components/sobreNosotros/sobreNosotros.js
@@ -0,0 +1,8 @@
+export const Nosotros = () => {
+    return (
+        <>
+        <h1>Sobre nosotros</h1>
+        <p>texto sobre nosotros</p>
+        </>
+    )
+}
---
+++ src/containers/banner.js
@@ -0,0 +1,7 @@
+import './banner.css'
+
+export function Banner () {
+    return (
+        <img alt="fabrica de papel" src="https://i.imgur.com/GrgNCjn.jpg" className="w100"/>
+    )
+}
