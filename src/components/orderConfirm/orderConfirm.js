945c9efef0540cc8fe6664ed9397af80300d6998
cl4ram <molinayvediaclara@outlook.com>
2021-11-28 12:17:39 -0300

    optimización

--- src/components/orderConfirm/orderConfirm.js
+++ src/components/orderConfirm/orderConfirm.js
@@ -5,26 +5,32 @@ export const OrderConfirm = ({id , close , confirm}) => {
     }
 
     return(
-        <>
-            <div className="modal"> 
-                <div className="modal-content"> 
-                    <div className="modal-header">
-                        <h1>Número de pedido</h1>
+        <div className="modal"> 
+            <div className="modal-content"> 
+                <div className="modal-header">
+                    <h1>Número de pedido</h1>
+                </div>
+                {id ? (
+                    <div>
+                        <div className="modal-body">
+                            <p>Su numero de orden es: {id}</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button onClick={close} className="button-after">¡Lo tengo!</button>
+                        </div>
                     </div>
-                    <div className="modal-body">
-                    {id ? (
-                        <p>Su numero de orden es: {id}</p>
                     ):(
-                        <p>Procesando pedido...</p>
+                        <div>
+                            <div className="modal-body">
+                                <p>Su numero de orden es: {id}</p>
+                        
+                            </div>
+                            <div className="modal-footer">
+                                <button onClick={close} className="button-after" disabled>¡Lo tengo!</button>
+                            </div>
+                        </div>
                     )}
-                
-                    </div>
-                    <div className="modal-footer">
-                        <button onClick={close} className="button-after">¡Lo tengo!</button>
-                    </div>
                 </div>
             </div>
-        
-        </>
     )
 }
